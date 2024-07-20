4869b9c3db65ccea1c793520851b76102fdd9646
michalgolda <michal.golda@hotmail.com>
2024-07-20 13:01:01 +0200

    chore: not select password field - make this as a default behaviour

--- controllers/userController.js
+++ controllers/userController.js
@@ -89,7 +89,7 @@ exports.login = async (req, res, next) => {
     try {
         const { email, password } = req.body;
 
-        const user = await User.findOne({ email });
+        const user = await User.findOne({ email }).select('password');
         if (!user) {
             throw new InvalidCredentialsError();
         }
